e0c2fd5561ad951906345fa221cd5bc337defe97
HanYoungsoo <gglife333@gmail.com>
2017-01-23 11:02:37 +0900

    InsertionSort edit!

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,9 +1,9 @@
 def Insertion_sort(un_list):
 
-    for idx, valueToInsert in enumerate(un_list):
+    for idx in range(len(un_list)):
         # select the hole position where number is to be inserted
-        print(idx, valueToInsert)
         holePosition = idx
+        valueToInsert = un_list[idx]
         # check if previous no. is larger than value to be inserted
         while holePosition > 0 and un_list[holePosition-1] > valueToInsert:
             un_list[holePosition - 1], un_list[holePosition] = un_list[holePosition], un_list[holePosition-1]
@@ -12,5 +12,5 @@ def Insertion_sort(un_list):
     return un_list
 
 un_list = [10,9,8,7,5,20,30]
-new_list = insertion_sort(un_list)
+new_list = Insertion_sort(un_list)
 print(new_list)
