1ffd319580495cd90cad36ff0a27fc609635e7be
HanYoungsoo <gglife333@gmail.com>
2017-01-30 22:04:47 +0900

    MergeSort.py edit

--- MergeSort.py
+++ MergeSort.py
@@ -5,13 +5,19 @@ def mergeSort(alist):
         mid = len(alist)//2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
+        print("lefthalf:", lefthalf)
+        print("righthalf:", righthalf)
 
+        print("1st start!")
         mergeSort(lefthalf)
+        print("2nd start!")
         mergeSort(righthalf)
+        print("righthalf2:", righthalf)
 
         i=0
         j=0
         k=0
+
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] < righthalf[j]:
                 alist[k]=lefthalf[i]
@@ -30,8 +36,10 @@ def mergeSort(alist):
             alist[k]=righthalf[j]
             j=j+1
             k=k+1
-    print("Merging ",alist)
 
-alist = [6,2,4,1,3,7,5,8]
+    print("Merging ", alist)
+    print("Merging end")
+
+alist = [10,2,20,1,9,0,44,7]
 mergeSort(alist)
 print(alist)
